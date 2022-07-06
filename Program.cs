78833a49ba6146be80cce4332e67b27751a7e97c
bruninhopsk <bruninhopsk@outlook.com>
2022-07-06 20:03:09 -0300

    Criado código para verificar se uma palavra é palindromo e desenho técnico de um cenario onde um worker le um arquivo txt e faz algum tipo de processamento

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace C_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe uma palavra");
+            string palavra = Console.ReadLine();
+
+            string palavraAoContrario = string.Empty;
+
+            var quantidadeDeCaracteres = palavra.Length;
+
+            for(var contador = quantidadeDeCaracteres; contador >=1; contador--)
+            {
+                palavraAoContrario += palavra[contador - 1].ToString();
+            }
+
+            int eUmaPalavraPalindromo = 0;
+
+            if (palavra.Equals(palavraAoContrario))
+            {
+                eUmaPalavraPalindromo = 1;
+
+                Console.WriteLine("1");
+            }
+            else
+            {
+                Console.WriteLine("0");
+            }
+        }
+    }
+}
